b023c525356bf8ea127a4383870f0457e9c84bc2
Arturo Blanco <ablanco@mentez.com>
2011-05-31 17:34:29 -0300

    Creating rails 3.1.0.rc1 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PinkNBlue::Application.config.secret_token = 'b42d591704458d78105176f2e3cb1ecf26ca8b7e2d89e08351f15a520c5c69d3c20621d4ab57e73d64cee7837e955c1d751bf5482edcdfacd9295923064a5966'
