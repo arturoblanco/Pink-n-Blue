abe1dce93eef466ffd62c8eae3ef69e56b43efd5
Arturo Blanco <arturoblanco700@gmail.com>
2011-05-31 23:06:32 -0300

    Adding rspec model tests, and validations to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  # VALIDATIONS
+  validates :name, :uniqueness => true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,19 +2,36 @@ require 'spec_helper'
 
 describe Product do
   context "Creating a product" do
+    before :each do
+      clean_models
+    end
     context "with valid information" do
       before :each do
-        @product = Factory.build(:product)
+        @product = Factory.build(:product, :name => "Product 1")
       end
       it "should save the product successfully" do
         @product.save.should == true
         @product.should have(0).errors
+        @product.name.should == "Product 1"
         Product.all.count.should == 1
       end
     end
     context "with invalid information" do
+      before :each do
+        product = Factory(:product, :name => "Produc 1")
+        @new_product = Factory.build(:product, :name => product.name)
+      end
       it "should validate uniqueness of name" do
+        @new_product.save.should == false
+        @new_product.should have(1).error_on(:name)
+        Product.all.count.should == 1
       end
     end
+  end
+
+  private
+
+  def clean_models
+    Product.destroy_all
   end
 end
