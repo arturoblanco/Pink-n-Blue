2f497807d6778c14215f857e27f1bd18bcb885bf
Arturo Blanco <arturoblanco700@gmail.com>
2011-08-19 03:06:13 -0300

    Changes

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,8 @@
 module ProductsHelper
+  def is_new?(product)
+    if (product.created_at + 2.weeks) >= Time.now
+      return content_tag :span, "Novo !!", :class => "new_product"
+    end
+    ""
+  end
 end
